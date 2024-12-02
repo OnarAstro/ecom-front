067428aedd70e5f5ec36a21a584daf4c0e7f3608
OnarAstro <zzeze1686@gmail.com>
2024-12-02 16:04:12 +0200

    first commit

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -1,7 +1,7 @@
 import "./Offer.css"
 const Offer = () => {
   return (
-    <section className="offer bg-cover bg-center w-full px-4  py-24 mt-16">
+    <section className="offer">
       <div className="container">
         <h2 className="h2">Summer Sale 50%</h2>
         <h3 className="h3 capitalize">Men&apos;s Leather Formal Wear Shoes </h3>
